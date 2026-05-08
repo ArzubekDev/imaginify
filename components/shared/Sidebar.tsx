e6fd39263348189f6696f4b88c6b57af7d1c6ee5
ArzubekDev <arzubekmain@gmail.com>
2026-05-08 15:55:39 +0600

    style: Sidebar

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
     <aside className="sidebar">
       <div className="flex flex-col size-full gap-4">
         <Link href={'/'} className="sidebar-logo">
-          <Image src={'/images/logo.png'} alt="logo" />
+          <Image src={'/images/logo.png'} alt="logo" width={100} height={100} />
         </Link>
       </div>
     </aside>
