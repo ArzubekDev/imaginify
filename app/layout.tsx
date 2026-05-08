b0bf28b6bccd5416447415150ea7dad6b134a10c
ArzubekDev <arzubekmain@gmail.com>
2026-05-08 15:09:38 +0600

    feat: sign-up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
-import type { Metadata } from "next";
-import { IBM_Plex_Sans } from "next/font/google";
-import { cn } from "@/lib/utils";
-import "./globals.css";
+import { cn } from '@/lib/utils';
+import type { Metadata } from 'next';
+import { IBM_Plex_Sans } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+import './globals.css';
 
-const IBMPlex = IBM_Plex_Sans({subsets:['latin'],weight: ["400", "500", "600", "700"], variable: "--font-ibm-plex"});
-
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex',
+});
 
 export const metadata: Metadata = {
-  title: "Imaginify",
-  description: "Imaginify is an AI-powered image generation tool that allows users to create stunning visuals from text prompts. With a user-friendly interface and advanced algorithms, Imaginify transforms your ideas into captivating images in seconds.",
+  title: 'Imaginify',
+  description:
+    'Imaginify is an AI-powered image generation tool that allows users to create stunning visuals from text prompts. With a user-friendly interface and advanced algorithms, Imaginify transforms your ideas into captivating images in seconds.',
 };
 
 export default function RootLayout({
@@ -17,11 +22,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={cn("font-IBMPlex antialiased", IBMPlex.variable)}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>
+        <body className="min-h-full flex flex-col">
+        {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
