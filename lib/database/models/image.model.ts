d6430aa1165f17d236f1cb1e5d76e355e48ce89c
ArzubekDev <arzubekmain@gmail.com>
2026-05-11 13:04:29 +0600

    feat: image.models.ts

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,45 @@
+import { model, models, Schema } from 'mongoose';
+
+import { Document } from 'mongoose';
+
+export interface IImage extends Document {
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureUrl: string;
+  width?: number;
+  height?: number;
+  config?: Record<string, any>;
+  transformationUrl?: URL;
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author?: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  createdAt: Date;
+  updateTag: Date;
+}
+
+const ImageSchema = new Schema({
+  title: { type: String, required: true },
+  transformationType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureUrl: { type: String, required: true },
+  width: { type: Number },
+  height: { type: Number },
+  config: { type: Object },
+  transformationUrl: { type: URL },
+  aspectRatio: { type: String },
+  color: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now },
+  updateTag: { type: Date, default: Date.now },
+});
+
+const Image = models?.Image || model('Image', ImageSchema);
+
+export default Image;
