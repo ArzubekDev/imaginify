1d44791cadc893d0fcca5fd5a839c5fd2b0addfc
ArzubekDev <arzubekmain@gmail.com>
2026-05-11 11:53:03 +0600

    feat: sheet

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
-  )
-}
+    <header className="">
+      <Link href={'/'} className="flex items-center gap-2 md:py-2">
+        <Image src={'/images/logo.png'} alt="Logo" width={30} height={30} />
+      </Link>
+    </header>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
