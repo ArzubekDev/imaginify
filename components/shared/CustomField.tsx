dfa7846d2787d0bfef4dc6ef89e043577b6d53b7
ArzubekDev <arzubekmain@gmail.com>
2026-05-12 11:29:48 +0600

    feat: CustomField

---
+++ components/shared/CustomField.tsx
@@ -0,0 +1,43 @@
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import z from 'zod';
+import {formSchema} from './TransformationForm';
+import { Control } from 'react-hook-form';
+
+type CustomFieldProps = {
+    control: Control<z.infer<typeof formSchema>> | undefined;
+    render: (props: {field: unknown }) => React.ReactNode;
+    name: keyof z.infer<typeof formSchema>;
+    formLabel?: string;
+    className?: string;
+}
+
+const CustomField = ({ control, render, name, formLabel, className }: CustomFieldProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className={className}>
+
+          {formLabel && <FormLabel>{formLabel}</FormLabel>}
+
+          <FormControl>
+           {render({field})}
+          </FormControl>
+
+          <FormDescription>Provide a concise title for your bug report.</FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default CustomField;
--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -6,19 +6,11 @@ import { z } from 'zod';
 
 import {
   Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
 } from '@/components/ui/form';
 
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import { defaultValues } from '@/constants';
 
-const formSchema = z.object({
+export const formSchema = z.object({
   title: z.string(),
   aspectRatio: z.string().optional(),
   color: z.string().optional(),
@@ -52,25 +44,7 @@ const TransformationForm = ({ action, data = null }: TransformationFormProps) =>
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Bug Title</FormLabel>
-
-              <FormControl>
-                <Input placeholder="Login button not working on mobile" {...field} />
-              </FormControl>
-
-              <FormDescription>Provide a concise title for your bug report.</FormDescription>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <Button type="submit">Submit</Button>
+        
       </form>
     </Form>
   );
