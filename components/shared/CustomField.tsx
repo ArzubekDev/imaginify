e6b25ea57fb3d3da2a6ac829d5cd574fad54039a
ArzubekDev <arzubekmain@gmail.com>
2026-05-12 12:01:12 +0600

    feat: select

--- components/shared/CustomField.tsx
+++ components/shared/CustomField.tsx
@@ -6,19 +6,42 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
-import z from 'zod';
-import {formSchema} from './TransformationForm';
-import { Control } from 'react-hook-form';
+import {
+  Control,
+  ControllerRenderProps,
+  FieldValues,
+  Path,
+} from 'react-hook-form';
 
-type CustomFieldProps = {
-    control: Control<z.infer<typeof formSchema>> | undefined;
-    render: (props: {field: unknown }) => React.ReactNode;
-    name: keyof z.infer<typeof formSchema>;
-    formLabel?: string;
-    className?: string;
-}
+// type CustomFieldProps = {
+//     control: Control<z.infer<typeof formSchema>> | undefined;
+//     render: (props: {field: unknown }) => React.ReactNode;
+//     name: keyof z.infer<typeof formSchema>;
+//     formLabel?: string;
+//     className?: string;
+// }
 
-const CustomField = ({ control, render, name, formLabel, className }: CustomFieldProps) => {
+type CustomFieldProps<T extends FieldValues> = {
+  control: Control<T>;
+  name: Path<T>;
+  formLabel: string;
+
+  render: ({
+    field,
+  }: {
+    field: ControllerRenderProps<T, Path<T>>;
+  }) => React.ReactNode;
+
+  className?: string;
+};
+
+const CustomField = <T extends FieldValues>({
+  control,
+  name,
+  formLabel,
+  render,
+  className,
+}: CustomFieldProps<T>) => {
   return (
     <FormField
       control={control}
--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -4,11 +4,11 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
-import {
-  Form,
-} from '@/components/ui/form';
+import { Form } from '@/components/ui/form';
 
 import { defaultValues } from '@/constants';
+import CustomField from './CustomField';
+import { Input } from '../ui/input';
 
 export const formSchema = z.object({
   title: z.string(),
@@ -44,7 +44,13 @@ const TransformationForm = ({ action, data = null }: TransformationFormProps) =>
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-        
+        <CustomField
+          control={form.control}
+          name="title"
+          formLabel="Title"
+          render={({ field }) => <Input {...field} className="input" />}
+          className="w-full"
+        />
       </form>
     </Form>
   );
