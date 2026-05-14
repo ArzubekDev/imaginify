e79664f5d0bc5ce1bdd26b88d0c22041d6def072
ArzubekDev <arzubekmain@gmail.com>
2026-05-14 15:35:09 +0600

    feat: ImageDetails

--- app/(root)/transformations/[id]/page.tsx
+++ app/(root)/transformations/[id]/page.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import Header from '@/components/shared/Header';
+import { getImageById } from '@/lib/actions/image.actions';
+import { auth } from '@clerk/nextjs/server';
 
-const TransformationsPage = () => {
+const ImageDetails = async ({ params }: SearchParamProps) => {
+  const { id } = await params;
+  const { sessionClaims } = await auth();
+  const userId = sessionClaims?.userId as string;
+
+  const image = await getImageById(id);
+
   return (
-    <div> TransformationsPage</div>
-  )
-}
+    <>
+      <Header title={image.title} />
 
-export default TransformationsPage
+      <section>
+        <div>
+          <p>Transformation:</p>
+          <p>{image.transformationType}</p>
+        </div>
+
+        {image.prompt && (
+          <>
+            <p>&#x25CF</p>
+            <div>
+              <p>Prompt:</p>
+              <p>{image.prompt}</p>
+            </div>
+          </>
+        )}
+
+        {image.color && (
+          <>
+           <p>&#x25CF</p>
+            <div>
+              <p>Color:</p>
+              <p>{image.color}</p>
+            </div>
+          </>
+        )}
+        {image.aspectRatio && (
+          <>
+           <p>&#x25CF</p>
+            <div>
+              <p>Aspect Ratio:</p>
+              <p>{image.aspectRatio}</p>
+            </div>
+          </>
+        )}
+      </section>
+    </>
+  );
+};
+
+export default ImageDetails;
