6555e03d08fe528ce88081561de16b104848a0ca
ArzubekDev <arzubekmain@gmail.com>
2026-05-14 16:24:42 +0600

    style: update

--- app/(root)/transformations/[id]/update/page.tsx
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -1,8 +1,37 @@
+import Header from '@/components/shared/Header';
+import TransformationForm from '@/components/shared/TransformationForm';
+import { transformationTypes } from '@/constants';
+import { getImageById } from '@/lib/actions/image.actions';
+import { getUserByClerkId } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const UpdateTransformationPage = () => {
+const UpdateTransformationPage = async ({params}: SearchParamProps) => {
+  const {id} = await params;
+  const {userId} = await auth();
+
+  if(!userId) redirect("/sign-in");
+  const user = await getUserByClerkId(userId);
+  const image = await getImageById(id);
+
+  const transformation = transformationTypes[image.transformationType as TransformationTypeKey]
+ 
   return (
-    <div>UpdateTransformationPage</div>
+    <>
+    <Header title={transformation.title} subtitle={transformation.subtitle}/>
+
+    <section className='mt-10'>
+<TransformationForm
+action='update'
+userId={user._id}
+type={image.transformationType as TransformationTypeKey}
+creditBalance={user.creditBalance}
+config={image.config}
+data={image}
+/>
+    </section>
+    </>
   )
 }
 
